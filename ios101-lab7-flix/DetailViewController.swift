d6aca7c1edab41bda4456f4e43a5f5b2c42048f6
Kayla Son <ksonkayla@gmail.com>
2023-10-28 01:27:52 -0700

    completed lab

--- ios101-lab7-flix/DetailViewController.swift
+++ ios101-lab7-flix/DetailViewController.swift
@@ -18,15 +18,50 @@ class DetailViewController: UIViewController {
 
     // TODO: Add favorite button outlet
 
+    @IBOutlet weak var favoriteButton: UIButton!
+    
     // TODO: Add favorite button action
-
+    @IBAction func didTapFavoriteButton(_ sender: UIButton) {
+        // Set the button's isSelected state to the opposite of it's current value.
+        sender.isSelected = !sender.isSelected
+        
+        // 1. If the button is in the *selected* state (i.e. "favorited") -> Add movie to favorites
+        // 2. Otherwise, the button is in the *un-selected* state (i.e."un-favorited") -> Remove movie from favorites
+        if sender.isSelected {
+            // 1.
+            movie.addToFavorites()
+        } else {
+            // 2.
+            movie.removeFromFavorites()
+        }
+    }
+    
     var movie: Movie!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // TODO: Update favorite button selected state
+        // Set the button's corner radius to be 1/2  it's width. This will make a square button round.
+        favoriteButton.layer.cornerRadius = favoriteButton.frame.width / 2
+        
+        // Update the button's selected state based on the current movie's favorited status.
+        // 1. Get the array of favorite movies.
+        // 2. Check if the favorite movies array contains the current movie.
+        // 3. If so, the movie has been favorited -> Set the button to the *selected* state.
+        // 4. Otherwise, the movie is not-favorited -> Set the button to the *un-selected* state.
+        // ------
 
+        // 1.
+        let favorites = Movie.getMovies(forKey: Movie.favoritesKey)
+        // 2.
+        if favorites.contains(movie) {
+            // 3.
+            favoriteButton.isSelected = true
+        } else {
+            // 4.
+            favoriteButton.isSelected = false
+        }
 
 
 
